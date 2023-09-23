f1fa87851b8e08684b85dbfeb1037856c28f61ed
Lu0-Wenfeng <vincent_lwf@hotmail.com>
2023-09-23 00:54:07 -0700

    Initialized Redux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,35 +6,39 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import UpdatePassword from "./pages/UpdatePassword";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   const [searchInput, setSearchInput] = useState("");
 
   return (
-    <Router>
-      <Grid
-        templateAreas={`"header" "main" "footer"`}
-        gridTemplateRows={"64px 1fr 85px"}
-        gridTemplateColumns={"100%"}
-        h="100vh"
-      >
-        <GridItem bg="#111827" area={"header"}>
-          <Header onSearch={(searchText) => setSearchInput(searchText)} />
-        </GridItem>
+    <Provider store={store}>
+      <Router>
+        <Grid
+          templateAreas={`"header" "main" "footer"`}
+          gridTemplateRows={"64px 1fr 85px"}
+          gridTemplateColumns={"100%"}
+          h="100vh"
+        >
+          <GridItem bg="#111827" area={"header"}>
+            <Header onSearch={(searchText) => setSearchInput(searchText)} />
+          </GridItem>
 
-        <GridItem bg="white" area={"main"}>
-          <Routes>
-            <Route path="/" element={<SignIn />} />
-            <Route path="/sign-in" element={<SignIn />} />
-            <Route path="/sign-up" element={<SignUp />} />
-            <Route path="/update-pwd" element={<UpdatePassword />} />
-          </Routes>
-        </GridItem>
-        <GridItem bg="#111827" area={"footer"}>
-          <Footer />
-        </GridItem>
-      </Grid>
-    </Router>
+          <GridItem bg="white" area={"main"}>
+            <Routes>
+              <Route path="/" element={<SignIn />} />
+              <Route path="/sign-in" element={<SignIn />} />
+              <Route path="/sign-up" element={<SignUp />} />
+              <Route path="/update-pwd" element={<UpdatePassword />} />
+            </Routes>
+          </GridItem>
+          <GridItem bg="#111827" area={"footer"}>
+            <Footer />
+          </GridItem>
+        </Grid>
+      </Router>
+    </Provider>
   );
 }
 
