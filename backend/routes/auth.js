4188fc7e27fe28a982f592101dd3c9bf6016c349
Lu0-Wenfeng <vincent_lwf@hotmail.com>
2023-09-26 18:31:04 -0700

    synced up routes names

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const { signup, signin, updatePassword } = require('../controllers/auth');
 const errorHandler = require("../controllers/error.js");
 
-router.post('/signup', signup);
-router.post('/signin', signin);
-router.post('/logout', logout)
-router.post('/update', updatePassword)
+router.post('/sign-up', signup);
+router.post('/sign-in', signin);
+// router.post('/logout', logout)
+router.post('/update-pwd', updatePassword)
 
 module.exports = router;
