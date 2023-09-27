75668638765cf4e0e20a37a77e8dadb796a07b7c
Lu0-Wenfeng <vincent_lwf@hotmail.com>
2023-09-26 18:11:59 -0700

    Minor tweaks of UI

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import {
   Box,
   Button,
@@ -13,40 +14,81 @@ import MyCard from "../components/MyCard";
 
 const SignIn = () => {
   const [show, setShow] = useState(false);
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+
   const handleClick = () => setShow(!show);
+  const onEmailChange = (e) => {
+    setEmail(e.target.value);
+    if (!e.target.value) {
+      setEmailError("This field is required");
+    } else if (!/\S+@\S+\.\S+/.test(e.target.value)) {
+      setEmailError("Invalid Email format");
+    } else {
+      setEmailError("");
+    }
+  };
+
+  const onPasswordChange = (e) => {
+    setPassword(e.target.value);
+    if (!e.target.value) {
+      setPasswordError("This field is required");
+    } else {
+      setPasswordError("");
+    }
+  };
 
   return (
     <Box display="flex" justifyContent="center" alignItems="center" h="100%">
       <MyCard title="Sign In to your account">
-        <Box mb="4">
-          <Text mb="2" textColor="gray">
-            Email:
-          </Text>
+        <Box mb="5">
+          <Text textColor="gray">Email:</Text>
           <Input
+            type="email"
+            value={email}
+            onChange={onEmailChange}
             placeholder="Enter your email"
+            mt="2"
             variant="outline"
-            my="4"
             border="1px solid"
-            borderColor="gray.300"
+            borderColor={emailError ? "red.500" : "gray.300"}
+            textColor="black"
+            autoFocus={true}
           />
+          {emailError && (
+            <Text color="red.500" fontSize="sm" float="right">
+              {emailError}
+            </Text>
+          )}
         </Box>
-        <Box mb="4">
-          <Text mb="2" textColor="gray">
-            Password:
-          </Text>
+        <Box mb="5">
+          <Text textColor="gray">Password:</Text>
           <InputGroup>
             <Input
               type={show ? "text" : "password"}
+              value={password}
+              onChange={onPasswordChange}
               placeholder="Enter your password"
+              mt="2"
               variant="outline"
-              my="4"
               border="1px solid"
               borderColor="gray.300"
+              textColor="black"
             />
-            <InputRightElement>
-              <Button onClick={handleClick}>{show ? "Hide" : "Show"}</Button>
+            <InputRightElement mt="5px" mr="15px">
+              <Button color="gray.500" onClick={handleClick}>
+                <Text decoration="underline">{show ? "Hide" : "Show"}</Text>
+              </Button>
             </InputRightElement>
           </InputGroup>
+          {passwordError && (
+            <Text color="red.500" fontSize="sm" mt="1">
+              {passwordError}
+            </Text>
+          )}
         </Box>
         <Button mb="2" bg="#5048E5" w="100%" textColor="white">
           Sign In
