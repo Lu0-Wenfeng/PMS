5a0c482c887faf4b1757eda0fbcbe35a23d55451
Lu0-Wenfeng <vincent_lwf@hotmail.com>
2023-09-27 23:22:29 -0700

    logout done

--- frontend/src/pages/LoginSuccess.jsx
+++ frontend/src/pages/LoginSuccess.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import { Text } from '@chakra-ui/react'
+import React from "react";
+import { Text, Button, VStack } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 const LoginSuccess = () => {
-	return (
-		<Text color="black">Login/Sign Up Successful</Text>
-	)
-}
+  const navigate = useNavigate();
 
-export default LoginSuccess
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+  };
+
+  return (
+    <VStack
+      display="flex"
+      direction="column"
+      justify="center"
+      h="100%"
+      spacing="20px"
+    >
+      <Text color="black">Login/Sign Up Successful</Text>
+      <Button colorScheme="red" onClick={handleLogout}>
+        Logout
+      </Button>
+    </VStack>
+  );
+};
+
+export default LoginSuccess;
