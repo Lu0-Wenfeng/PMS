387bded468738e563f5b5ab2c820188fb9356efa
Lu0-Wenfeng <vincent_lwf@hotmail.com>
2023-10-03 13:18:36 -0700

    modified styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,20 @@ import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 
+
 import Footer from "./components/Layout/Footer";
 import Header from "./components/Layout/Header";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import LoginSuccess from "./pages/LoginSuccess";
 import UpdatePassword from "./pages/UpdatePassword";
 import CreateProductPage from "./pages/CreateProductPage";
 import AllProductsPage from "./pages/AllProductsPage";
 import ProductDetailPage from "./pages/ProductDetailPage";
 
 import store from "./store";
-import LoginSuccess from "./pages/LoginSuccess";
+
 
 
 
