806b60cda49b46f996155e96cddeaf6a911ce8ed
Lu0-Wenfeng <vincent_lwf@hotmail.com>
2023-10-03 17:23:01 -0700

    added timestamps

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,31 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    // TODO: Categroty
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    inStockQuantity: {
+      type: Number,
+      required: true,
+    },
+    productImageUrl: {
+      type: String,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-	// TODO: Categroty
-	category: {},
-  price: {
-    type: Number,
-    required: true
-  },
-  inStockQuantity: {
-    type: Number,
-		required: true
-  },
-	productImageUrl: {
-    type: String
-  },
-});
+  { timestamps: true }
+);
 
-
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 
 module.exports = Product;
