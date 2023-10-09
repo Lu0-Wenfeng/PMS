289e2e6569221f2c4993457a6fc62c0e0a2dbbb3
Lu0-Wenfeng <vincent_lwf@hotmail.com>
2023-10-08 19:13:25 -0700

    Changed to "Go Back"

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { VStack, Flex, Text, Button } from "@chakra-ui/react";
-import { WarningIcon } from '@chakra-ui/icons'
+import { WarningIcon } from "@chakra-ui/icons";
+import { useSelector } from "react-redux";
 
 const ErrorPage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <Flex height="100vh" alignItems="center" justifyContent="center">
-            <VStack spacing="4">
-                <WarningIcon w={58} h={58} color="blue.500" />
-                <Text fontSize="xl" fontWeight="bold" color="gray.700">Oops! Something went wrong.</Text>
-                <Button colorScheme="blue" onClick={() => navigate('/')}>Go Home</Button>
-            </VStack>
-        </Flex>
-    );
-}
+  return (
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <VStack spacing="4">
+        <WarningIcon w={58} h={58} color="blue.500" />
+        <Text fontSize="xl" fontWeight="bold" color="gray.700">
+          Oops! Something went wrong.
+        </Text>
+        <Button colorScheme="blue" onClick={() => navigate(-1)}>
+          Go Back
+        </Button>
+      </VStack>
+    </Flex>
+  );
+};
 
 export default ErrorPage;
