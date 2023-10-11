1fac56ebf43628e6d92a6c10f10d88d351064f10
Lu0-Wenfeng <vincent_lwf@hotmail.com>
2023-10-10 22:35:16 -0700

    optimized thunks

--- frontend/src/store/authSlice.js
+++ frontend/src/store/authSlice.js
@@ -1,15 +1,26 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { syncCartWithServer } from "./cartSlice";
 import axios from "axios";
+
+const initialState = {
+  isAdmin: false,
+  isLoggedIn: false,
+  email: "",
+  emailError: "",
+  passwordError: "",
+  unknownError: "",
+};
 
 export const userSignIn = createAsyncThunk(
   "auth/userSignIn",
-  async ({ email, password }, { rejectWithValue }) => {
+  async ({ email, password }, { rejectWithValue, dispatch, getState }) => {
     try {
       const response = await axios.post("http://localhost:3000/sign-in", {
         email: email,
         password: password,
       });
-      // console.log(response.data);
+      localStorage.setItem("token", response.data.token);
+      dispatch(syncCartWithServer(getState().cart.items));
       return response.data;
     } catch (error) {
       if (!error.response) {
@@ -22,14 +33,19 @@ export const userSignIn = createAsyncThunk(
 
 export const userSignUp = createAsyncThunk(
   "auth/userSignUp",
-  async ({ email, password, isAdmin }, { rejectWithValue }) => {
+  async (
+    { email, password, isAdmin },
+    { rejectWithValue, dispatch, getState }
+  ) => {
     console.log("Sign Up:", email, password, isAdmin);
     try {
-      const response = await axios.post("http://localhost:3000/sign-up", {
+      const response = await axios.post("http://localhost:3000/sync-cart", {
         email: email,
         password: password,
         isAdmin: isAdmin,
       });
+      localStorage.setItem("token", response.data.token);
+      dispatch(syncCartWithServer(getState().cart.items));
       return response.data;
     } catch (error) {
       if (!error.response) {
@@ -40,14 +56,14 @@ export const userSignUp = createAsyncThunk(
   }
 );
 
-const initialState = {
-  isAdmin: false,
-  isLoggedIn: false,
-  email: "",
-  emailError: "",
-  passwordError: "",
-  unknownError: "",
-};
+export const handleLogout = createAsyncThunk(
+  "auth/handleLogout",
+  async (_, { dispatch, getState }) => {
+    await dispatch(syncCartWithServer(getState().cart.items));
+    dispatch(userLoggedOut());
+    localStorage.removeItem("token");
+  }
+);
 
 const authSlice = createSlice({
   name: "auth",
@@ -63,14 +79,14 @@ const authSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(userSignIn.fulfilled, (state, action) => {
-        localStorage.setItem("token", action.payload.token);
         state.isLoggedIn = true;
         state.isAdmin = action.payload.isAdmin;
         state.email = action.payload.email;
+        //sync cart
       })
       .addCase(userSignIn.rejected, (state, action) => {
         if (!action.payload) {
-          state.unknownError = action.payload.message;
+          state.unknownError = "unkown error";
         } else if (action.payload.message === "User Not exist") {
           state.emailError = action.payload.message;
         } else if (action.payload.message === "Incorrect Password") {
