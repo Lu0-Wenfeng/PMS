a157921a3d0dc62c5713d38025a5edf817d369a1
Lu0-Wenfeng <vincent_lwf@hotmail.com>
2023-10-17 15:07:55 -0700

    Router protection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,28 @@
 import { Grid, GridItem } from "@chakra-ui/react";
-import { useSelector } from "react-redux";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
   Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes
 } from "react-router-dom";
 
 import Footer from "./components/Layout/Footer";
 import Header from "./components/Layout/Header";
-import Home from "./pages/Home";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import LoginSuccess from "./pages/LoginSuccess";
-import UpdatePassword from "./pages/UpdatePassword";
-import CreateProductPage from "./pages/CreateProductPage";
 import AllProductsPage from "./pages/AllProductsPage";
-import ProductDetailPage from "./pages/ProductDetailPage";
+import CreateProductPage from "./pages/CreateProductPage";
 import EditProductPage from "./pages/EditProduct";
 import ErrorPage from "./pages/ErrorPage";
+import LoginSuccess from "./pages/LoginSuccess";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import UpdatePassword from "./pages/UpdatePassword";
 
 function App() {
   const isAdmin = useSelector((state) => state.auth.isAdmin);
   const [searchInput, setSearchInput] = useState("");
-
-  const isAuthenticated = () => {
-    // 根据你的认证逻辑，检查用户是否登录并且是管理员
-    return isAdmin === "admin";
-  };
-
-  const [navigate, setNavigate] = useState(null);
-
-  // 路由保护函数，如果用户不是管理员，将其重定向到/sign-in页面
-  const adminRouteProtection = (element) => {
-    if (!isAuthenticated() && navigate) {
-      navigate("/sign-in");
-      return null;
-    }
-    return element;
-  };
 
   return (
     <Router>
@@ -56,10 +39,7 @@ function App() {
 
         <GridItem backgroundColor="gray.100" area={"main"}>
           <Routes>
-            <Route
-              path="/"
-              element={<Navigate to="/all-products" replace />}
-            />
+            <Route path="/" element={<Navigate to="/all-products" replace />} />
             <Route path="/error" element={<ErrorPage />} />
             <Route path="/sign-in" element={<SignIn />} />
             <Route path="/sign-up" element={<SignUp />} />
@@ -67,21 +47,19 @@ function App() {
             <Route path="/success" element={<LoginSuccess />} />
             <Route
               path="/create-product"
-              element={adminRouteProtection(<CreateProductPage />)}
+              element={isAdmin ? <CreateProductPage /> : <ErrorPage />}
             />
             <Route path="/all-products" element={<AllProductsPage />} />
-            <Route
-              path="/all-products/:id"
-              element={<ProductDetailPage />}
-            />
+            <Route path="/all-products/:id" element={<ProductDetailPage />} />
             <Route
               path="/search-product/:query"
               element={<AllProductsPage />}
             />
             <Route
               path="/edit-product/:id"
-              element={adminRouteProtection(<EditProductPage />)}
+              element={isAdmin ? <EditProductPage /> : <ErrorPage />}
             />
+            <Route path="*" element={<Navigate to="/error" replace />} />
           </Routes>
         </GridItem>
         <GridItem bg="#111827" area={"footer"}>
--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -1,8 +1,7 @@
+import { WarningIcon } from "@chakra-ui/icons";
+import { Button, Flex, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { VStack, Flex, Text, Button } from "@chakra-ui/react";
-import { WarningIcon } from "@chakra-ui/icons";
-import { useSelector } from "react-redux";
 
 const ErrorPage = () => {
   const navigate = useNavigate();
@@ -14,7 +13,13 @@ const ErrorPage = () => {
         <Text fontSize="xl" fontWeight="bold" color="gray.700">
           Oops! Something went wrong.
         </Text>
-        <Button colorScheme="blue" onClick={() => navigate(-1)}>
+        <Button
+          colorScheme="blue"
+          onClick={() => {
+            console.log("Oops! Something went wrong");
+            navigate(-1);
+          }}
+        >
           Go Back
         </Button>
       </VStack>
